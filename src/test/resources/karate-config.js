93ee0ee135a8bfc258c1ea6b3e7280c7f29e5578
Lance Kind <lancerkind@yahoo.com>
2026-03-04 14:02:42 -0600

    Cleaned up readme to reflect update.

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,14 +1,3 @@
 function fn() {
-  var env = karate.env;
-  var Setup;
-  if (env == 'spanner') {
-    Setup = Java.type('samplespannerapplication.testcontainers.ContainerSetupSpanner');
-  } else {
-    Setup = Java.type('sampleoracleapplication.testcontainers.ContainerSetupOracle');
-  }
-  return {
-    jdbcUrl: Setup.getJdbcUrl(),
-    username: Setup.getUsername(),
-    password: Setup.getPassword()
-  };
+
 }
